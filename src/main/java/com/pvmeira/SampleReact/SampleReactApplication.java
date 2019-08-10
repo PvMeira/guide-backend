d87e6a090123decd1f9d03f0a6e57d6dbb6f37d8
Pedro Vitor <pv.ciria@gmail.com>
2019-08-10 10:11:42 -0300

    First Commit

---
+++ src/main/java/com/pvmeira/SampleReact/SampleReactApplication.java
@@ -0,0 +1,13 @@
+package com.pvmeira.SampleReact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleReactApplication.class, args);
+	}
+
+}
